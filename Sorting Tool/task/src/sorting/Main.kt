a6457aa866b4bcc2228383008e86ab654efdb755
ntshntsh <evsyelly@gmail.com>
2022-12-12 12:30:38 +0300

    add main cases and output

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -1,29 +1,29 @@
 package sorting
 
-import java.util.Comparator
 import java.util.Scanner
 
-//const val ARG_DATATYPE = "-dataType"
+const val ARG_DATA = "-dataType"
 const val MODE_LONG = "long"
 const val MODE_LINE = "line"
-//const val MODE_WORD = "word"
+const val MODE_WORD = "word"
 
-const val ARG_SORT = "-sortIntegers"
-//const val ARG_SORT = "-sortingType"
+const val SORT_MODE_COUNT = "byCount"
 
 fun main(args: Array<String>) {
 
-    if (ARG_SORT in args){
-        MODE_LONG.sortItOut()
-    }
-    else notJustNumbers(args[1])
+    val mode = if (ARG_DATA in args)
+        args[args.indexOf(ARG_DATA) + 1]
+    else MODE_WORD
 
+    if (SORT_MODE_COUNT in args)
+        printData(mode, read(mode).sortByCount(mode))
+    else printData(mode, read(mode).sort(mode))
 }
 
 //stage  3/6 task
 private fun String.sortItOut() {
     val data = read(this).sort(this)
-    printData(data)
+    printData(this, data)
 }
 
 //stage 2/6 task
@@ -34,13 +34,41 @@ private fun notJustNumbers(mode: String) {
     printData(mode, data, max, timesOfMax)
 }
 
-private fun printData(
-    data: List<String>
-) {
-    println("Total numbers: ${data.count()}")
-    println("Sorted data: ${data.joinToString(" ")}")
+private fun String.getUnitName(): String {
+    return when (this) {
+        MODE_LONG -> "number"
+        MODE_LINE -> "line"
+        else -> "word"
+    }
 }
 
+private fun printData(
+    mode: String,
+    data: Map<String, Int>
+) {
+    val sum = data.values.sum()
+    println("Total ${mode.getUnitName()}s: $sum")
+    data.forEach {
+        println("${it.key}: ${it.value} time(s), ${100.0 * it.value / sum}%")
+    }
+
+}
+
+private fun printData(
+    mode: String,
+    data: List<String>
+) {
+
+    val separator = when (mode) {
+        MODE_LINE -> "\n"
+        else -> " "
+    }
+
+    println("Total ${mode.getUnitName()}s: ${data.count()}")
+    println("Sorted data: ${data.joinToString(separator)}")
+}
+
+//stage 2/6 output
 private fun printData(
     mode: String,
     data: List<String>,
@@ -66,15 +94,23 @@ private fun printData(
     )
 }
 
+private fun List<String>.sortByCount(mode: String): Map<String, Int> {
+    val sortedData = mutableMapOf<String, Int>()
+
+    return sortedData
+}
+
+//stage 3/6 sorting
 private fun List<String>.sort(mode: String): List<String> {
     val list = this.toMutableList()
     if (mode == MODE_LONG) {
         list.sortBy { it.toInt() }
     } else {
-        list.sortWith(Comparator
-            .comparing<String?, Int?> { it.length }
-            .thenComparing(Comparator.comparing { it })
-        )
+        list.sort()
+        //list.sortWith(Comparator
+        //    .comparing<String?, Int?> { it.length }
+        //    .thenComparing(Comparator.comparing { it })
+        //)
     }
     return list
 }
