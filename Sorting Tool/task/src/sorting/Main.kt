62b028a75d60021e7082b215ef12be8352c60947
ntshntsh <evsyelly@gmail.com>
2022-12-12 15:34:57 +0300

    final result (change sorting via comparator for groupingBy)

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -16,22 +16,8 @@ fun main(args: Array<String>) {
     else MODE_WORD
 
     if (SORT_MODE_COUNT in args)
-        printData(mode, read(mode).sortByCount(mode))
-    else printData(mode, read(mode).sort(mode))
-}
-
-//stage  3/6 task
-private fun String.sortItOut() {
-    val data = read(this).sort(this)
-    printData(this, data)
-}
-
-//stage 2/6 task
-private fun notJustNumbers(mode: String) {
-    val data = read(mode).sort(mode)
-    val max = data.last()
-    val timesOfMax = data.count { it == max }
-    printData(mode, data, max, timesOfMax)
+        read(mode).sortByCount(mode).printData(mode)
+    else read(mode).sort(mode).printData(mode)
 }
 
 private fun String.getUnitName(): String {
@@ -42,76 +28,41 @@ private fun String.getUnitName(): String {
     }
 }
 
-private fun printData(
-    mode: String,
-    data: Map<String, Int>
-) {
-    val sum = data.values.sum()
+private fun Map<String, Int>.printData(mode: String) {
+    val sum = this.values.sum()
     println("Total ${mode.getUnitName()}s: $sum")
-    data.forEach {
-        println("${it.key}: ${it.value} time(s), ${100.0 * it.value / sum}%")
+    this.forEach {
+        println("${it.key}: ${it.value} time(s), ${100 * it.value / sum}%")
     }
-
 }
 
-private fun printData(
-    mode: String,
-    data: List<String>
-) {
+private fun List<String>.printData(mode: String) {
 
     val separator = when (mode) {
         MODE_LINE -> "\n"
         else -> " "
     }
 
-    println("Total ${mode.getUnitName()}s: ${data.count()}")
-    println("Sorted data: ${data.joinToString(separator)}")
-}
-
-//stage 2/6 output
-private fun printData(
-    mode: String,
-    data: List<String>,
-    max: String,
-    timesOfMax: Int
-) {
-    val unitNameMsg = when (mode) {
-        MODE_LONG -> "number"
-        else -> "word"
-    }
-    val unitMaxMsg = when (mode) {
-        MODE_LONG -> "greatest"
-        else -> "longest"
-    }
-    val suffix = when (mode) {
-        MODE_LINE -> "\n"
-        else -> ""
-    }
-    println(
-        """Total ${unitNameMsg}s: ${data.count()}.
-        |The $unitMaxMsg $unitNameMsg: $suffix$max$suffix ($timesOfMax time(s), ${100 * timesOfMax / data.count()}%).
-        """.trimMargin()
-    )
+    println("Total ${mode.getUnitName()}s: ${this.count()}")
+    println("Sorted data: ${this.joinToString(separator)}")
 }
 
 private fun List<String>.sortByCount(mode: String): Map<String, Int> {
-    val sortedData = mutableMapOf<String, Int>()
-
-    return sortedData
+    val map = if (mode == MODE_LONG) {
+        this.sortedBy { it.toInt() }.groupingBy { it }.eachCount()
+    } else
+        this.sorted().groupingBy { it }.eachCount()
+    return map.toList().sortedBy { (_, v) -> v }.toMap()
 }
 
 //stage 3/6 sorting
 private fun List<String>.sort(mode: String): List<String> {
     val list = this.toMutableList()
-    if (mode == MODE_LONG) {
+    if (mode == MODE_LONG)
         list.sortBy { it.toInt() }
-    } else {
+    else
         list.sort()
-        //list.sortWith(Comparator
-        //    .comparing<String?, Int?> { it.length }
-        //    .thenComparing(Comparator.comparing { it })
-        //)
-    }
+
     return list
 }
 
